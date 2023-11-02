156fc85cbe591c9b2baace110674fd96ea91c49c
Olin <olin@Olins-MBP.hsd1.co.comcast.net>
2023-11-01 18:08:29 -0600

    remove unused

--- index.ts
+++ index.ts
@@ -25,7 +25,7 @@ app.use('/users', usersRouter);
 
 
 // catch 404 and forward to error handler
-app.use(function (req, res, next) {
+app.use(function (req: Request, res: Response, next: NextFunction) {
     next(createError(404));
 });
 
