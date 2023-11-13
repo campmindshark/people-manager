899188eb505793eb5facf461bd9d8706d4a8f77c
Olin <olin@Olins-MBP.hsd1.co.comcast.net>
2023-11-12 17:49:57 -0700

    fix refs

--- packages/backend/routes/users.ts
+++ packages/backend/routes/users.ts
@@ -1,5 +1,5 @@
 import express, { Request, Response, NextFunction, Router } from 'express';
-import User from '../user/user';
+import User from '../models/user/user';
 
 const router: Router = express.Router();
 
