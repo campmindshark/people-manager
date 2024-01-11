dc8dedca88465175d209129b732b845699f87b59
dtp263 <olin.davis20@gmail.com>
2024-01-10 20:58:27 -0700

    fix health endpoint

--- packages/backend/routes/index.ts
+++ packages/backend/routes/index.ts
@@ -3,14 +3,14 @@ import path from 'path';
 
 const router: Router = express.Router();
 
+router.get('/health', (req: Request, res: Response) => {
+  res.status(200).send('healthy');
+});
+
 /* GET home page. */
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 router.get('/*', (req: Request, res: Response, next: NextFunction) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
-});
-
-router.get('/health', (req: Request, res: Response) => {
-  res.status(200).send('healthy');
 });
 
 export default router;
