728132de1a2a46eccf847e5ee27c19ed299cb215
dtp263 <olin.davis20@gmail.com>
2024-01-10 19:44:47 -0700

    fix json

--- terraform/main.tf
+++ terraform/main.tf
@@ -107,12 +107,14 @@ resource "aws_iam_policy" "container_access_policy" {
 {
   "Version": "2012-10-17",
   "Statement": [
-  {
-    "Effect": "Allow",
-    "Action": [
-      "secretsmanager:GetSecretValue"
-    ]
-  }
+    {
+      "Effect": "Allow",
+      "Action": [
+        "secretsmanager:GetSecretValue"
+      ],
+      "Resource": "*"
+    }
+  ]
 }
 EOF
 }
