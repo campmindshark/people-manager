a2a9e4e6bd270780e76e7e5dcaf3ad568b5b80af
dtp263 <olin.davis20@gmail.com>
2024-01-10 20:21:14 -0700

    set target group port to 3001

--- terraform/main.tf
+++ terraform/main.tf
@@ -172,7 +172,7 @@ resource "aws_security_group" "load_balancer_security_group" {
 
 resource "aws_lb_target_group" "target_group" {
   name        = var.project_name
-  port        = 80
+  port        = 3001
   protocol    = "HTTP"
   target_type = "ip"
   vpc_id      = aws_default_vpc.default_vpc.id # Referencing the default VPC
