0227455340f11a0b7718413f6fa8f7fedbb1b722
dtp263 <olin.davis20@gmail.com>
2024-01-11 20:10:11 -0700

    add bucket policy

--- terraform/s3/s3.tf
+++ terraform/s3/s3.tf
@@ -8,6 +8,25 @@ resource "aws_s3_bucket" "bucket" {
   bucket = "${var.project_name}-bucket-0"
 }
 
+
+resource "aws_s3_bucket_policy" "bucket_policy" {
+  bucket = aws_s3_bucket.bucket.id
+  policy = jsonencode(
+    {
+      "Version" : "2012-10-17",
+      "Statement" : [
+        {
+          "Sid" : "PublicReadGetObject",
+          "Effect" : "Allow",
+          "Principal" : "*",
+          "Action" : "s3:GetObject",
+          "Resource" : "arn:aws:s3:::${aws_s3_bucket.bucket.id}/*"
+        }
+      ]
+    }
+  )
+}
+
 resource "aws_s3_bucket_website_configuration" "hosting" {
   bucket = aws_s3_bucket.bucket.id
 
