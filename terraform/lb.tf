ed341b06b81812e341a726091ef5c06d4ddcec9e
dtp263 <olin.davis20@gmail.com>
2024-02-01 23:12:51 -0700

    revert

--- terraform/lb.tf
+++ terraform/lb.tf
@@ -27,8 +27,8 @@ resource "aws_alb" "application_load_balancer" {
 # Creating a security group for the load balancer:
 resource "aws_security_group" "load_balancer_security_group" {
   ingress {
-    from_port   = var.https_port
-    to_port     = var.port_number
+    from_port   = 80
+    to_port     = 80
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"] # Allowing traffic in from all sources
   }
