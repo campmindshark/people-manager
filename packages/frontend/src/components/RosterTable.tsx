9d965031cea35d25637bb3925ed4eaf7450832a9
dtp263 <olin.davis20@gmail.com>
2024-03-11 18:12:20 -0600

    add remaining fields to roster

--- packages/frontend/src/components/RosterTable.tsx
+++ packages/frontend/src/components/RosterTable.tsx
@@ -10,6 +10,20 @@ import { useRecoilValue } from 'recoil';
 import RosterParticipantViewModel from 'backend/view_models/roster_participant';
 import { CurrentRosterParticipantsState } from '../state/roster';
 
+const skillsString = (skills: string[]): string => {
+  if (!skills || skills.length === 0) {
+    return '';
+  }
+  let skillString = '';
+  for (let i = 0; i < skills.length; i += 1) {
+    skillString += skills[i];
+    if (i < skills.length - 1) {
+      skillString += ', ';
+    }
+  }
+  return skillString;
+};
+
 function RosterTable() {
   const participants = useRecoilValue(CurrentRosterParticipantsState);
 
@@ -18,7 +32,20 @@ function RosterTable() {
       <TableHead>
         <TableRow>
           <TableCell>Name</TableCell>
+          <TableCell>Playa Name</TableCell>
+          <TableCell>Location</TableCell>
           <TableCell>Probability of Attending</TableCell>
+
+          <TableCell>Ticket</TableCell>
+          <TableCell>Extra Tickets</TableCell>
+
+          <TableCell>Years Attended</TableCell>
+          <TableCell>Years At Camp</TableCell>
+          <TableCell>Referral Name</TableCell>
+          <TableCell>Sleeping Arrangement</TableCell>
+          <TableCell>Skills</TableCell>
+          <TableCell>Interested in EA</TableCell>
+          <TableCell>Interested in Post Burn</TableCell>
         </TableRow>
       </TableHead>
       <TableBody>
@@ -31,7 +58,43 @@ function RosterTable() {
               {participant.user.firstName} {participant.user.lastName}
             </TableCell>
             <TableCell component="th" scope="row">
+              {participant.user.playaName}
+            </TableCell>
+            <TableCell component="th" scope="row">
+              {participant.user.location}
+            </TableCell>
+            <TableCell component="th" scope="row">
               {participant.rosterParticipant.probabilityOfAttending}
+            </TableCell>
+            <TableCell component="th" scope="row">
+              {participant.rosterParticipant.hasTicket ? 'Yes' : 'No'}
+            </TableCell>
+            <TableCell component="th" scope="row">
+              {participant.rosterParticipant.extraTickets ? 'Yes' : 'No'}
+            </TableCell>
+            <TableCell component="th" scope="row">
+              {participant.rosterParticipant.yearsAttended}
+            </TableCell>
+            <TableCell component="th" scope="row">
+              {participant.rosterParticipant.yearsAtCamp.join(', ')}
+            </TableCell>
+            <TableCell component="th" scope="row">
+              {participant.user.referralName}
+            </TableCell>
+            <TableCell component="th" scope="row">
+              {participant.rosterParticipant.sleepingArrangement}
+            </TableCell>
+            <TableCell component="th" scope="row">
+              {skillsString(participant.user.skillsOfNote)},
+              {participant.user.skillsNotInList}
+            </TableCell>
+            <TableCell component="th" scope="row">
+              {participant.rosterParticipant.earlyArrivalInterest
+                ? 'Yes'
+                : 'No'}
+            </TableCell>
+            <TableCell component="th" scope="row">
+              {participant.rosterParticipant.postBurnInterest ? 'Yes' : 'No'}
             </TableCell>
           </TableRow>
         ))}
