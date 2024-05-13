34f1e2c03e8ad566ae8d2ab400fc67ec62974662
Olin Davis <olin.davis20@gmail.com>
2024-05-13 17:32:28 -0600

    Add Roster stats bar for convenient data things (#32)
    * add Stat component, add roster stats row
    
    * add interested in EA to stats

---
+++ packages/frontend/src/components/RosterStats.tsx
@@ -0,0 +1,62 @@
+import React, { useCallback } from 'react';
+import { Grid } from '@mui/material';
+import { useRecoilValue } from 'recoil';
+import { CurrentRosterParticipantsState } from '../state/roster';
+import Stat from './stats/Stat';
+
+function RosterStats() {
+  const participants = useRecoilValue(CurrentRosterParticipantsState);
+
+  const getYearsOfExperience = useCallback(
+    () =>
+      participants.reduce(
+        (acc, p) => acc + p.rosterParticipant.yearsAttended,
+        0,
+      ),
+    [participants],
+  );
+
+  const getPercentageOfVirgins = useCallback(
+    () =>
+      participants.filter((p) => p.rosterParticipant.yearsAttended === 0)
+        .length / participants.length,
+    [participants],
+  );
+
+  const hasTickets = useCallback(
+    () => participants.filter((p) => p.rosterParticipant.hasTicket).length,
+    [participants],
+  );
+
+  const interestedInEA = useCallback(
+    () =>
+      participants.filter((p) => p.rosterParticipant.earlyArrivalInterest)
+        .length,
+    [participants],
+  );
+
+  return (
+    <Grid container spacing={2}>
+      <Grid item>
+        <Stat value={participants.length} unit="Participants" />
+      </Grid>
+      <Grid item>
+        <Stat value={getYearsOfExperience()} unit="Years of Experience" />
+      </Grid>
+      <Grid item>
+        <Stat
+          value={(getPercentageOfVirgins() * 100).toFixed(2)}
+          unit="% Virgins"
+        />
+      </Grid>
+      <Grid item>
+        <Stat value={hasTickets()} unit="Has Ticket" />
+      </Grid>
+      <Grid item>
+        <Stat value={interestedInEA()} unit="Interested in EA" />
+      </Grid>
+    </Grid>
+  );
+}
+
+export default RosterStats;
