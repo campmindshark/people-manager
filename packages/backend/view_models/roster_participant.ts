e2c1eec4620ae53441c4d72ee757b54b18bf11f2
dtp263 <olin.davis20@gmail.com>
2024-06-10 21:25:27 -0600

    lint

--- packages/backend/view_models/roster_participant.ts
+++ packages/backend/view_models/roster_participant.ts
@@ -1,5 +1,5 @@
-import User from '../models/user/user';
 import PrivateProfile from 'models/user/user_private';
+import User from '../models/user/user';
 import RosterParticipant from '../models/roster_participant/roster_participant';
 
 export default interface RosterParticipantViewModel {
